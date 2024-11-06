17476d595e9f27c8e94b2c71271b1f6d7f4823bd
Sanskar Dixit <48208274+dixitsanskar@users.noreply.github.com>
2024-11-06 22:17:00 +0530

    Update db_connection.py

--- db_connection.py
+++ db_connection.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient  
 
-url = 'mongodb+srv://root:S@nCybySama@cluster0.lq35h.mongodb.net'
+url = 'mongodb+srv://root:RHVvLbi5tJgqUxzm@cluster0.lq35h.mongodb.net'
 MONGO_CLIENT = MongoClient(url)
 
 MONGO_DB = MONGO_CLIENT['grader'] 
